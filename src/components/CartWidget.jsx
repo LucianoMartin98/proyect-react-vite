8a09c008d5f67aa187a5f7038e95257b35e20136
Luciano Nahuel Martin <lucianonahuelmartin98@hotmail.com>
2022-07-21 17:54:01 -0300

    Inclusion de CartWidget y ItemListContainer

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CartWidget = () => {
+    return (
+        <>
+            <li class="nav-item">
+                <a class="nav-link"><i class="bi bi-cart"></i></a>
+            </li>
+        </>
+    )
+}
+
+export default CartWidget
